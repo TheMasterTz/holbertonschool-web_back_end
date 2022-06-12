2a7b684c1dbbd5dfe961dbf6282172781d6d3e55
Christian Diaz <84691244+TheMasterTz@users.noreply.github.com>
2022-06-11 21:36:10 -0500

    task advanced

--- 0x03-caching/100-lfu_cache.py
+++ 0x03-caching/100-lfu_cache.py
@@ -1,49 +1,68 @@
 #!/usr/bin/python3
-""" LFU Caching """
-from base_caching import BaseCaching
-from collections import OrderedDict
+''' LFUCache '''
+
+
+BaseCaching = __import__('base_caching').BaseCaching
 
 
 class LFUCache(BaseCaching):
-    """ Class that inherits from BaseCaching and is a caching system """
+    ''' LFU Cache '''
+
     def __init__(self):
+        ''' Initialize LFUCache '''
+        self.aux_list = []
+        self.aux_dict = {}
         super().__init__()
-        self.lru_cache = OrderedDict()
-        self.lfu_cache = {}
+
+    def _get_less_frequency(self):
+        """
+        It returns the minimum value of the dictionary.
+        :return: The minimum value in the dictionary.
+        """
+        return min(self.aux_dict.values())
 
     def put(self, key, item):
-        """ Assign to the dictionary, LFU algorithm """
-        if key in self.lru_cache:
-            del self.lru_cache[key]
-        if len(self.lru_cache) > BaseCaching.MAX_ITEMS - 1:
-            min_value = min(self.lfu_cache.values())
-            lfu_keys = [k for k, v in self.lfu_cache.items() if v == min_value]
-            if len(lfu_keys) == 1:
-                print("DISCARD:", lfu_keys[0])
-                self.lru_cache.pop(lfu_keys[0])
-                del self.lfu_cache[lfu_keys[0]]
+        """
+        If the key and item are not empty, and the cache is full,
+        remove the least frequently used item, and add the new key and item
+        to the cache
+
+        :param key: the key to be added to the cache
+        :param item: the item to be added to the cache
+        """
+        if key and item:
+            if self.cache_data.__len__() >= BaseCaching.MAX_ITEMS:
+                less_frequency = self._get_less_frequency()
+                keys_with_less_frequency = [key for key, value
+                                            in self.aux_dict.items()
+                                            if value == less_frequency]
+                if keys_with_less_frequency.__len__() > 1:
+                    keys_with_less_frequency.sort()
+                    key_to_remove = keys_with_less_frequency[0]
+                else:
+                    key_to_remove = keys_with_less_frequency[0]
+                self.aux_list.remove(key_to_remove)
+                del self.aux_dict[key_to_remove]
+                del self.cache_data[key_to_remove]
+                print(f'DISCARD: {key_to_remove}')
+
+            if self.aux_dict.get(key):
+                self.aux_dict[key] += 1
             else:
-                for k, _ in list(self.lru_cache.items()):
-                    if k in lfu_keys:
-                        print("DISCARD:", k)
-                        self.lru_cache.pop(k)
-                        del self.lfu_cache[k]
-                        break
-        self.lru_cache[key] = item
-        self.lru_cache.move_to_end(key)
-        if key in self.lfu_cache:
-            self.lfu_cache[key] += 1
-        else:
-            self.lfu_cache[key] = 1
-        self.cache_data = dict(self.lru_cache)
+                self.aux_dict[key] = 1
+
+            self.aux_list.append(key)
+            self.cache_data[key] = item
 
     def get(self, key):
-        """ Return the value linked """
-        if key in self.lru_cache:
-            value = self.lru_cache[key]
-            self.lru_cache.move_to_end(key)
-            if key in self.lfu_cache:
-                self.lfu_cache[key] += 1
-            else:
-                self.lfu_cache[key] = 1
-            return value
+        """
+        If the key is in the cache, then return the value of the key
+
+        :param key: The key to be searched in the cache
+        :return: The value of the key in the cache_data dictionary.
+        """
+        if key in self.cache_data and key:
+            self.aux_dict[key] += 1
+            self.aux_list.remove(key)
+            self.aux_list.append(key)
+            return self.cache_data[key]
