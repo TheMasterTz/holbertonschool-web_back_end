06580eaba40e7e1d4ec88f966474939a0143638b
Christian Diaz <84691244+TheMasterTz@users.noreply.github.com>
2022-06-21 17:42:18 -0500

    all files

--- 0x06-Basic_authentication/api/v1/views/__init__.py
+++ 0x06-Basic_authentication/api/v1/views/__init__.py
@@ -3,5 +3,8 @@
 """
 from flask import Blueprint
 from api.v1.views.index import *
+from api.v1.views.users import *
 
 app_views = Blueprint("app_views", __name__, url_prefix="/api/v1")
+
+User.load_from_file()
--- 0x06-Basic_authentication/api/v1/views/index.py
+++ 0x06-Basic_authentication/api/v1/views/index.py
@@ -1,8 +1,7 @@
 #!/usr/bin/env python3
 """ Module of Index views
 """
-from os import abort
-from flask import Flask, jsonify
+from flask import jsonify, abort
 from api.v1.views import app_views
 
 
@@ -12,10 +11,28 @@ def status() -> str:
     Return:
       - the status of the API
     """
-    return jsonify({'status': 'OK'})
+    return jsonify({"status": "OK"})
 
 
 @app_views.route('/unauthorized', methods=['GET'], strict_slashes=False)
 def unauthorized() -> str:
     """ endpoint to testing (unauthorized) 401 error handler """
     abort(401)
+
+
+@app_views.route('/forbidden', methods=['GET'], strict_slashes=False)
+def forbidden() -> str:
+    """ endpoint to testing (forbidden) 403 error handler """
+    abort(403)
+
+
+@app_views.route('/stats/', strict_slashes=False)
+def stats() -> str:
+    """ GET /api/v1/stats
+    Return:
+      - the number of each objects
+    """
+    from models.user import User
+    stats = {}
+    stats['users'] = User.count()
+    return jsonify(stats)
