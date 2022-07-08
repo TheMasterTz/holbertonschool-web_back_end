d2abdba243583986b4e6fb277660dd3b0b4bf1b2
Christian Diaz <84691244+TheMasterTz@users.noreply.github.com>
2022-07-08 15:51:20 -0500

    all files

---
+++ 0x0A-i18n/2-app.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+""" Basic Flask app, Basic Babel setup """
+from flask import Flask, render_template, request
+from flask_babel import Babel
+
+app = Flask(__name__)
+babel = Babel(app)
+""" instantiate the Babel object """
+
+
+class Config(object):
+    """"sumary_line"""
+    LANGUAGES = ['en', 'fr']
+    BABEL_DEFAULT_LOCALE = 'en'
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
+
+
+app.config.from_object(Config)
+""" Use that class as config for Flask app """
+
+
+@babel.localeselector
+def get_locale():
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
+@app.route('/')
+def index():
+    """ basic Flask app """
+    return render_template('0-index.html')
+
+
+if __name__ == "__main__":
+    app.run()
