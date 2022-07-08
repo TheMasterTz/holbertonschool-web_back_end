e4b82e0db742d53a10684814779d49e0d416916e
Christian Diaz <84691244+TheMasterTz@users.noreply.github.com>
2022-07-08 15:52:25 -0500

    Update 2-app.py

--- 0x0A-i18n/2-app.py
+++ 0x0A-i18n/2-app.py
@@ -21,6 +21,7 @@ app.config.from_object(Config)
 
 @babel.localeselector
 def get_locale():
+    """ to determine the best match with our supported languages """
     return request.accept_languages.best_match(app.config['LANGUAGES'])
 
 
