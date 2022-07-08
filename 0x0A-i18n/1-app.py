a25c7d1e54c5565f3589d7d4fe223a29cb901861
Christian Diaz <84691244+TheMasterTz@users.noreply.github.com>
2022-07-08 16:00:37 -0500

    Update 1-app.py

--- 0x0A-i18n/1-app.py
+++ 0x0A-i18n/1-app.py
@@ -9,7 +9,7 @@ babel = Babel(app)
 
 
 class Config(object):
-    """"sumary_line"""
+    """ config class """
     LANGUAGES = ['en', 'fr']
     BABEL_DEFAULT_LOCALE = 'en'
     BABEL_DEFAULT_TIMEZONE = 'UTC'
@@ -22,7 +22,7 @@ app.config.from_object(Config)
 @app.route('/')
 def index():
     """ basic Flask app """
-    return render_template('0-index.html')
+    return render_template("1-index.html")
 
 
 if __name__ == "__main__":
